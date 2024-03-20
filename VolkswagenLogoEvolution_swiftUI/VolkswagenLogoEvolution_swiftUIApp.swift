0a924629aa9c8de522cddf592bc385cc8460e007
Ethan <q40131125@gmail.com>
2024-03-20 17:14:47 +0800

    Initial Commit

---
+++ VolkswagenLogoEvolution_swiftUI/VolkswagenLogoEvolution_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolkswagenLogoEvolution_swiftUIApp.swift
+//  VolkswagenLogoEvolution_swiftUI
+//
+//  Created by Ethan Hung on 2024/3/20.
+//
+
+import SwiftUI
+
+@main
+struct VolkswagenLogoEvolution_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
