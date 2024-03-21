22beeb6fe2178bd29e8573bcb8677407a34b55fc
Ethan <q40131125@gmail.com>
2024-03-21 15:23:04 +0800

    NavigationStack .tint update

---
+++ VolkswagenLogoEvolution_swiftUI/ColorExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExtensions.swift
+//  VolkswagenLogoEvolution_swiftUI
+//
+//  Created by Ethan Hung on 2024/3/21.
+//
+
+import SwiftUI
+
+struct ColorExtensions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorExtensions()
+}
---
+++ VolkswagenLogoEvolution_swiftUI/LogoView.swift
@@ -0,0 +1,18 @@
+//
+//  LogoView.swift
+//  VolkswagenLogoEvolution_swiftUI
+//
+//  Created by Ethan Hung on 2024/3/20.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoView()
+}
